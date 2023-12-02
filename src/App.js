8889c03258f075a535ee96b48011c38b3f5c8e07
dblackstone1 <147945739+dblackstone1@users.noreply.github.com>
2023-12-02 13:34:45 -0500

    Working app for deployment (#28)
    Co-authored-by: Natnael Amanuel <nathanielamanuel@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
-import NavBar from "./components/NavBar";
-import './App.css';
-import BottomNavBar from "./components/BottomNavBar";
+import logo from './logo.svg';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LocationHandler from "./components/LocationHandler";
 import {NavOptions} from "./utils/optionValues";
+import NavBar from "./components/NavBar";
+import BottomNavBar from "./components/BottomNavBar";
 import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 import TestResults from "./components/TestResults";
 import DemoPage from "./components/DemoPage";
-import LocationHandler from "./components/LocationHandler";
-import NotFound from "./components/NotFound";
 
-const App = () => {
+
+
+
+import './App.css';
+
+function App() {
   return (
     <BrowserRouter basename="/">
       <LocationHandler />
@@ -19,13 +23,13 @@ const App = () => {
         <Routes>
           <Route path={NavOptions.HOME} element={<Home />} />
           <Route path={NavOptions.TEST_RESULTS} element={<TestResults />} />
-          <Route path={NavOptions.DEMO} element={<DemoPage />} />
+          <Route path={NavOptions.DEMO} element={<DemoPage/>} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <BottomNavBar />
       </div>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
