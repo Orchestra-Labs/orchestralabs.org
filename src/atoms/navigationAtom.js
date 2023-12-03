b0d9adfa1b12ebe67572706f1a2f1b3d2c1f1f1f
dblackstone1 <dblackstone@orchestralabs.org>
2023-12-03 15:21:25 -0500

    Updates

--- src/atoms/navigationAtom.js
+++
@@ -1,9 +0,0 @@
-import { atom } from 'recoil';
-import {NavOptions} from '../utils/optionValues';
-
-export const navigationState = atom({
-  key: 'navigationState',
-  default: {
-    currentPage: NavOptions.HOME,
-  },
-});
