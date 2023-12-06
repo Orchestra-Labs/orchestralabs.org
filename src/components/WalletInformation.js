50266b909f3f4771576715a5ed0e130236522471
dblackstone1 <dblackstone@orchestralabs.org>
2023-12-05 23:25:34 -0500

    text updates

--- src/components/WalletInformation.js
+++ src/components/WalletInformation.js
@@ -16,11 +16,15 @@ const WalletInformation = () => {
             return <p>EMPTY</p>;
         }
     
-        return Object.entries(balances).map(([assetJSON, amount]) => {
-            const asset = JSON.parse(assetJSON);
-            return <p key={asset.symbol}>{`${asset.name} (${asset.symbol}): ${amount.toFixed(8)}`}</p>;
-        });
-    };
+        return (
+            <ul>
+                {Object.entries(balances).map(([assetJSON, amount]) => {
+                    const asset = JSON.parse(assetJSON);
+                    return <li key={asset.symbol}>{`${asset.name} (${asset.symbol}): ${amount.toFixed(8)}`}</li>;
+                })}
+            </ul>
+        );
+    };    
     
     return (
         <>
@@ -28,11 +32,20 @@ const WalletInformation = () => {
             <p>Next data update in: {infoUpdateCountdown}s. Blocks will finalize every 1-3 minutes.</p>
             <p>Give two blocks for blockchain to react to environmental changes.</p>
             <div>
-                <span className="data-label">Wallet Address:</span> <span className="data-value">{demoWallet.address}</span><br />
-                <span className="data-label">Wallet Balances:</span> <span className="data-value">{renderBalances(demoWallet.balances)}</span><br />
-                <span className="data-label">Exchange Balances:</span> <span className="data-value">{renderBalances(exchangeBalances)}</span><br />
-                <span className="data-label">Reserve Balances:</span> <span className="data-value">{renderBalances(reserveBalances)}</span><br />
-                <span className="data-label">Collateral Requirement:</span> <span className="data-value">{collateralRequirement}</span><br />
+                <span className="data-label">Wallet Address:</span> 
+                <span className="data-value">{demoWallet.address}</span><br />
+                
+                <span className="data-label">Wallet Balances:</span> 
+                <span className="data-value">{renderBalances(demoWallet.balances)}</span><br />
+                
+                <span className="data-label">Exchange Balances:</span> 
+                <span className="data-value">{renderBalances(exchangeBalances)}</span><br />
+                
+                <span className="data-label">Reserve Balances:</span> 
+                <span className="data-value">{renderBalances(reserveBalances)}</span><br />
+                
+                <span className="data-label">Collateral Requirement:</span> 
+                <span className="data-value">{collateralRequirement}</span><br />
             </div>
         </>
     );
