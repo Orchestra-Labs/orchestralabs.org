12bdf5da0eed91d660fcb3be7c00e1964e532915
dblackstone1 <dblackstone@orchestralabs.org>
2023-12-09 17:29:25 -0500

    feat: add background transition to black for video

--- src/components/DemoPage.js
+++ src/components/DemoPage.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import demoVideo from '../assets/videos/demo.mp4'; 
+import playButton from '../assets/images/play-button.svg';
 import "./DemoPage.css"
 
 const DemoPage = () => {
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        const handleVideoPlay = () => {
+            document.querySelector(".video-container").style.backgroundColor = "black";
+        };
+
+        const videoElement = videoRef.current;
+        videoElement.addEventListener("play", handleVideoPlay);
+
+        return () => {
+            videoElement.removeEventListener("play", handleVideoPlay);
+        };
+    }, []);
+
     return (
         <div className="video-container">
-            <video controls>
+            <video ref={videoRef} controls poster={playButton}>
                 <source src={demoVideo} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
