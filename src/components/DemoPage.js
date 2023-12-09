a1666d04eda6739b2330dc8ed1dc7247f216cdb0
dblackstone1 <dblackstone@orchestralabs.org>
2023-12-09 16:21:55 -0500

    fix: adapt video to screen size

--- src/components/DemoPage.js
+++ src/components/DemoPage.js
@@ -4,8 +4,8 @@ import "./DemoPage.css"
 
 const DemoPage = () => {
     return (
-        <div className="container">
-            <video width="1920" height="1080" controls>
+        <div className="video-container">
+            <video controls>
                 <source src={demoVideo} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
