66e3c0e4fbcf5b36a2b2b1d0c48d359483f82104
dblackstone1 <147945739+dblackstone1@users.noreply.github.com>
2024-04-15 08:52:56 -0400

    feat [SB-224]: update social links with real values (#45)

--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -3,35 +3,33 @@ import { FlexProps } from '@mantine/core/lib';
 import React from 'react';
 
 import { Discord } from '@/assets/icons/Discord';
-import { Facebook } from '@/assets/icons/Facebook';
+// import { Facebook } from '@/assets/icons/Facebook';
 import { Telegram } from '@/assets/icons/Telegram';
 import { XIcon } from '@/assets/icons/X';
-import { Youtube } from '@/assets/icons/Youtube';
+// import { Youtube } from '@/assets/icons/Youtube';
 import { design } from '@/theme/design';
 
 interface SocialLinksProps extends FlexProps {
   iconFill?: string;
 }
 
+const SOCIAL_LINKS = [
+  { id: 1, url: "https://twitter.com/orchestra_labs", Icon: XIcon },
+  { id: 2, url: "https://t.me/+xFieHCYYyx41NGQx", Icon: Telegram },
+  // { id: 3, url: "#", Icon: Youtube },
+  // { id: 3, url: "#", Icon: Facebook },
+  { id: 3, url: "https://discord.gg/3fvGM779", Icon: Discord }
+];
+
 export const SocialLinks: React.FC<SocialLinksProps> = ({
   iconFill = design.colors.showWhite,
   ...props
 }) => (
   <Flex align="center" gap="lg" {...props}>
-    <Anchor display="inline-flex" href="#" target="_blank">
-      <XIcon color={iconFill} />
-    </Anchor>
-    <Anchor display="inline-flex" href="#" target="_blank">
-      <Telegram color={iconFill} />
-    </Anchor>
-    <Anchor display="inline-flex" href="#" target="_blank">
-      <Youtube color={iconFill} />
-    </Anchor>
-    <Anchor display="inline-flex" href="#" target="_blank">
-      <Facebook color={iconFill} />
-    </Anchor>
-    <Anchor display="inline-flex" href="#" target="_blank">
-      <Discord color={iconFill} />
-    </Anchor>
+    {SOCIAL_LINKS.map(link => (
+      <Anchor key={link.id} display="inline-flex" href={link.url} target="_blank">
+        <link.Icon color={iconFill} />
+      </Anchor>
+    ))}
   </Flex>
 );
