6d08e3125fc6e3940f6abf441edf9cd2fafd6247
dblackstone1 <dblackstone@orchestralabs.org>
2024-04-19 01:42:11 -0400

    feat: add whitepaper link, comment out documentation

--- src/components/NavItems/NavItems.tsx
+++ src/components/NavItems/NavItems.tsx
@@ -9,10 +9,10 @@ type NavItemProps = Pick<FlexProps, 'visibleFrom' | 'gap' | 'direction'> & {
 };
 
 const LINKS = [
-  { id: 1, label: 'Whitepaper', url: '#' },
-  { id: 2, label: 'Documentation', url: '#' },
-  { id: 3, label: 'Explorer', url: '#' },
-  { id: 4, label: 'Blogs', url: 'https://medium.com/@orchestra_labs' },
+  { id: 1, label: 'Whitepaper', url: 'https://docs.google.com/document/d/1t6mzvrnWpbq3wyUlts9HUTcLE97Y6XUol8a_bJJTUlg/edit?usp=sharing' },
+  // { id: 2, label: 'Documentation', url: '#' },
+  { id: 2, label: 'Explorer', url: '#' },
+  { id: 3, label: 'Blogs', url: 'https://medium.com/@orchestra_labs' },
 ];
 
 export const NavItems: React.FC<NavItemProps> = ({ linkColor, ...props }) => (
