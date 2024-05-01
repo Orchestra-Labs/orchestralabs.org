7506e1b980293abfc6dab9995772c433c2a07a5d
Vasyl Stasiuk <vasyl.stasiuk@wetelo.com>
2024-05-01 17:16:50 +0300

    feat: added SignIn, SignUp and PasswordRecovery pages

---
+++ src/pages/explorer/PasswordRecovery/PasswordRecovery.tsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+
+import { ROUTES } from '@/config/routes';
+import { Button, Input } from '@/ui-kit';
+
+export const PasswordRecovery = () => (
+  <div className="page-container">
+    <div className="border border-neutral-3 rounded-[20px] mx-auto w-full max-w-[520px] py-9 px-14 flex flex-col items-center">
+      <h1 className="text-h4 md:text-h3 xl:text-h1 text-white font-semibold mb-3">
+        Password recovery
+      </h1>
+      <p className="text-base xl:text-lg text-neutral-1 text-center text-balance">
+        Enter the email address you used when you joined and we’ll send you
+        instruction to reset your password.
+      </p>
+      <form className="mt-9 flex-1 w-full">
+        <Input
+          variant="primary"
+          className="w-full h-12 py-2.5 px-3"
+          wrapperClass="mb-4"
+          label="Email Address"
+          placeholder="Your Email Address"
+        />
+      </form>
+      <Button className="w-full mt-10 text-lg">Reset Password</Button>
+      <Button variant="secondary" className="w-full mt-5 text-lg" asChild>
+        <Link
+          to={ROUTES.EXPLORER.SIGN_IN}
+          className="text-blue hover:text-blue-darker"
+        >
+          Back to Sign In
+        </Link>
+      </Button>
+    </div>
+  </div>
+);
---
+++ src/pages/explorer/SignIn/SignIn.tsx
@@ -0,0 +1,49 @@
+import { Link } from 'react-router-dom';
+
+import { EyeOpen } from '@/assets/icons';
+import { ROUTES } from '@/config/routes';
+import { Button, Input } from '@/ui-kit';
+
+export const SignIn = () => (
+  <div className="page-container">
+    <div className="border border-neutral-3 rounded-[20px] mx-auto w-full max-w-[520px] py-9 px-14 flex flex-col items-center">
+      <h1 className="text-h4 md:text-h3 xl:text-h1 text-white font-semibold mb-3">
+        Sign In
+      </h1>
+      <p className="text-base xl:text-lg text-neutral-1">
+        Already have an account?{' '}
+        <Link
+          to={ROUTES.EXPLORER.SIGN_UP}
+          className="text-blue hover:text-blue-darker"
+        >
+          Sign Up
+        </Link>
+      </p>
+      <form className="mt-9 flex-1 w-full">
+        <Input
+          variant="primary"
+          className="w-full h-12 py-2.5 px-3"
+          wrapperClass="mb-4"
+          label="Username"
+          placeholder="e.g. tomsmith"
+        />
+        <Input
+          variant="primary"
+          className="w-full h-12 py-2.5 px-3"
+          wrapperClass="mb-4"
+          label="Password"
+          placeholder="Password"
+          icon={<EyeOpen width={20} />}
+          iconRole="button"
+        />
+      </form>
+      <Button className="w-full mt-10 text-lg">Login</Button>
+      <Link
+        to={ROUTES.EXPLORER.PASSWORD_RECOVERY}
+        className="text-base md:text-lg text-blue hover:text-blue-darker mt-5"
+      >
+        Forgot password?
+      </Link>
+    </div>
+  </div>
+);
