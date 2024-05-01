0fd1a3535b749a9a862270d59c7bf04c909355b3
Vasyl Stasiuk <vasyl.stasiuk@wetelo.com>
2024-05-01 17:20:12 +0300

    feat: connected ExplorerLayout and newly added pages into Router

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -2,9 +2,24 @@ import React, { lazy } from 'react';
 import { Navigate, useRoutes } from 'react-router-dom';
 
 import { ROUTES } from '@/config/routes';
-import { Home } from '@/pages';
+import {
+  AccountDetails,
+  AllBlocks,
+  AllTokens,
+  AllTransactions,
+  BlockDetails,
+  ExplorerMain,
+  Home,
+  PasswordRecovery,
+  SignIn,
+  SignUp,
+  TransactionDetails,
+} from '@/pages';
 
 const MainLayout = lazy(() => import('../layouts/MainLayout/MainLayout'));
+const ExplorerLayout = lazy(
+  () => import('../layouts/ExplorerLayout/ExplorerLayout'),
+);
 
 export const AppRouter: React.FC = (): React.ReactElement | null =>
   useRoutes([
@@ -15,6 +30,52 @@ export const AppRouter: React.FC = (): React.ReactElement | null =>
         {
           path: ROUTES.HOME,
           element: <Home />,
+        },
+      ],
+    },
+    {
+      path: ROUTES.EXPLORER.ROOT,
+      element: <ExplorerLayout />,
+      children: [
+        {
+          path: ROUTES.EXPLORER.ROOT,
+          element: <ExplorerMain />,
+        },
+        {
+          path: ROUTES.EXPLORER.ACCOUNT_DETAILS,
+          element: <AccountDetails />,
+        },
+        {
+          path: ROUTES.EXPLORER.TOKENS,
+          element: <AllTokens />,
+        },
+        {
+          path: ROUTES.EXPLORER.BLOCKS,
+          element: <AllBlocks />,
+        },
+        {
+          path: ROUTES.EXPLORER.TRANSACTIONS,
+          element: <AllTransactions />,
+        },
+        {
+          path: ROUTES.EXPLORER.TRANSACTION_DETAILS,
+          element: <TransactionDetails />,
+        },
+        {
+          path: ROUTES.EXPLORER.BLOCK_DETAILS,
+          element: <BlockDetails />,
+        },
+        {
+          path: ROUTES.EXPLORER.SIGN_IN,
+          element: <SignIn />,
+        },
+        {
+          path: ROUTES.EXPLORER.SIGN_UP,
+          element: <SignUp />,
+        },
+        {
+          path: ROUTES.EXPLORER.PASSWORD_RECOVERY,
+          element: <PasswordRecovery />,
         },
       ],
     },
--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,3 +1,17 @@
+const EXPLORER_ROOT = '/explorer';
+
 export const ROUTES = {
   HOME: '/',
+  EXPLORER: {
+    ROOT: EXPLORER_ROOT,
+    ACCOUNT_DETAILS: `${EXPLORER_ROOT}/account/:id`,
+    TOKENS: `${EXPLORER_ROOT}/tokens`,
+    BLOCKS: `${EXPLORER_ROOT}/blocks`,
+    TRANSACTIONS: `${EXPLORER_ROOT}/transactions`,
+    TRANSACTION_DETAILS: `${EXPLORER_ROOT}/transactions/:id`,
+    BLOCK_DETAILS: `${EXPLORER_ROOT}/blocks/:id`,
+    SIGN_IN: `${EXPLORER_ROOT}/sign-in`,
+    SIGN_UP: `${EXPLORER_ROOT}/sign-up`,
+    PASSWORD_RECOVERY: `${EXPLORER_ROOT}/password-recovery`,
+  },
 };
