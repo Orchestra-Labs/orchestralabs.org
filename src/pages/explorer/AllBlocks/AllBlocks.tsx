14ab6afa8bcb046a123f057561ffd7bf7e51ef19
Vasyl Stasiuk <vasyl.stasiuk@wetelo.com>
2024-05-01 17:13:21 +0300

    feat: added AllBlocks and BlockDetails pages

---
+++ src/pages/explorer/AllBlocks/AllBlocks.tsx
@@ -0,0 +1,254 @@
+import { Link } from 'react-router-dom';
+
+import { Copy } from '@/assets/icons';
+import {
+  Progress,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/ui-kit';
+
+const TABLE_DATA = [
+  {
+    id: 1,
+    block: '19632593',
+    age: '4 days ago',
+    txn: '100',
+    from: 'beaverbuild',
+    gasUsed: '10,108,274 (33.69% | - 33%)',
+    gasLimit: '30,000,000',
+    baseFee: '37,94 Gwei',
+    reward: '0.05829 ETH',
+    burnsFees: {
+      value: '0.053592',
+      percents: '89.31%',
+    },
+  },
+  {
+    id: 2,
+    block: '19632593',
+    age: '4 days ago',
+    txn: '100',
+    from: 'beaverbuild',
+    gasUsed: '10,108,274 (33.69% | - 33%)',
+    gasLimit: '30,000,000',
+    baseFee: '37,94 Gwei',
+    reward: '0.05829 ETH',
+    burnsFees: {
+      value: '0.053592',
+      percents: '89.31%',
+    },
+  },
+  {
+    id: 3,
+    block: '19632593',
+    age: '4 days ago',
+    txn: '100',
+    from: 'beaverbuild',
+    gasUsed: '10,108,274 (33.69% | - 33%)',
+    gasLimit: '30,000,000',
+    baseFee: '37,94 Gwei',
+    reward: '0.05829 ETH',
+    burnsFees: {
+      value: '0.053592',
+      percents: '89.31%',
+    },
+  },
+  {
+    id: 4,
+    block: '19632593',
+    age: '4 days ago',
+    txn: '100',
+    from: 'beaverbuild',
+    gasUsed: '10,108,274 (33.69% | - 33%)',
+    gasLimit: '30,000,000',
+    baseFee: '37,94 Gwei',
+    reward: '0.05829 ETH',
+    burnsFees: {
+      value: '0.053592',
+      percents: '89.31%',
+    },
+  },
+  {
+    id: 5,
+    block: '19632593',
+    age: '4 days ago',
+    txn: '100',
+    from: 'beaverbuild',
+    gasUsed: '10,108,274 (33.69% | - 33%)',
+    gasLimit: '30,000,000',
+    baseFee: '37,94 Gwei',
+    reward: '0.05829 ETH',
+    burnsFees: {
+      value: '0.053592',
+      percents: '89.31%',
+    },
+  },
+  {
+    id: 6,
+    block: '19632593',
+    age: '4 days ago',
+    txn: '100',
+    from: 'beaverbuild',
+    gasUsed: '10,108,274 (33.69% | - 33%)',
+    gasLimit: '30,000,000',
+    baseFee: '37,94 Gwei',
+    reward: '0.05829 ETH',
+    burnsFees: {
+      value: '0.053592',
+      percents: '89.31%',
+    },
+  },
+  {
+    id: 7,
+    block: '19632593',
+    age: '4 days ago',
+    txn: '100',
+    from: 'beaverbuild',
+    gasUsed: '10,108,274 (33.69% | - 33%)',
+    gasLimit: '30,000,000',
+    baseFee: '37,94 Gwei',
+    reward: '0.05829 ETH',
+    burnsFees: {
+      value: '0.053592',
+      percents: '89.31%',
+    },
+  },
+  {
+    id: 8,
+    block: '19632593',
+    age: '4 days ago',
+    txn: '100',
+    from: 'beaverbuild',
+    gasUsed: '10,108,274 (33.69% | - 33%)',
+    gasLimit: '30,000,000',
+    baseFee: '37,94 Gwei',
+    reward: '0.05829 ETH',
+    burnsFees: {
+      value: '0.053592',
+      percents: '89.31%',
+    },
+  },
+  {
+    id: 9,
+    block: '19632593',
+    age: '4 days ago',
+    txn: '100',
+    from: 'beaverbuild',
+    gasUsed: '10,108,274 (33.69% | - 33%)',
+    gasLimit: '30,000,000',
+    baseFee: '37,94 Gwei',
+    reward: '0.05829 ETH',
+    burnsFees: {
+      value: '0.053592',
+      percents: '89.31%',
+    },
+  },
+  {
+    id: 10,
+    block: '19632593',
+    age: '4 days ago',
+    txn: '100',
+    from: 'beaverbuild',
+    gasUsed: '10,108,274 (33.69% | - 33%)',
+    gasLimit: '30,000,000',
+    baseFee: '37,94 Gwei',
+    reward: '0.05829 ETH',
+    burnsFees: {
+      value: '0.053592',
+      percents: '89.31%',
+    },
+  },
+];
+
+export const AllBlocks = () => (
+  <div className="page-container">
+    <h1 className="text-h1 text-white font-semibold">Blocks</h1>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mt-7.5 gap-5">
+      <div className="flex flex-col w-full border border-neutral-2 p-5 pb-2.5 rounded-lg bg-background-dark-grey">
+        <span className="text-base text-neutral-1">
+          NETWORK UTILIZATION (24H)
+        </span>
+        <span className="text-h5 text-white font-semibold mt-1">50.6%</span>
+      </div>
+      <div className="flex flex-col w-full border border-neutral-2 p-5 pb-2.5 rounded-lg bg-background-dark-grey">
+        <span className="text-base text-neutral-1">LAST SAFE BLOCK</span>
+        <span className="text-h5 text-white font-semibold mt-1">19633349</span>
+      </div>
+      <div className="flex flex-col w-full border border-neutral-2 p-5 pb-2.5 rounded-lg bg-background-dark-grey">
+        <span className="text-base text-neutral-1">
+          PRODUCED BY MEV BUILDERS( 24H)
+        </span>
+        <span className="text-h5 text-white font-semibold mt-1">88.4%</span>
+      </div>
+      <div className="flex flex-col w-full border border-neutral-2 p-5 pb-2.5 rounded-lg bg-background-dark-grey">
+        <span className="text-base text-neutral-1">BURNT FEES 🔥</span>
+        <span className="text-h5 text-white font-semibold mt-1">
+          4,254,203.22 ETH
+        </span>
+      </div>
+    </div>
+    <div className="flex flex-col mt-7.5 w-full border border-neutral-2 p-5 pb-2.5 rounded-lg bg-background-dark-grey">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Block</TableHead>
+            <TableHead>Age</TableHead>
+            <TableHead>Txn</TableHead>
+            <TableHead>From</TableHead>
+            <TableHead>Gas Used</TableHead>
+            <TableHead>Gas Limit</TableHead>
+            <TableHead>Base Fee</TableHead>
+            <TableHead>Reward</TableHead>
+            <TableHead>Burns Fees (ETH)</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {TABLE_DATA.map(item => (
+            <TableRow key={item.id}>
+              <TableCell>
+                <Link to="#" className="text-blue hover:text-blue-darker">
+                  {item.block}
+                </Link>
+              </TableCell>
+              <TableCell>{item.age}</TableCell>
+              <TableCell>
+                <Link to="#" className="text-blue hover:text-blue-darker">
+                  {item.txn}
+                </Link>
+              </TableCell>
+              <TableCell className="inline-flex items-center">
+                <Link to="#" className="text-blue hover:text-blue-darker">
+                  {item.from}
+                </Link>
+                <Copy
+                  className="ml-2.5 text-neutral-1"
+                  width={18}
+                  height={18}
+                />
+              </TableCell>
+              <TableCell>
+                <div className="flex flex-col px-5">
+                  <span>{item.gasUsed}</span>
+                  <Progress className="w-full mt-1.5" value={30} />
+                </div>
+              </TableCell>
+              <TableCell>{item.gasLimit}</TableCell>
+              <TableCell>{item.gasUsed}</TableCell>
+              <TableCell>{item.reward}</TableCell>
+              <TableCell className="flex items-center">
+                <span>{item.burnsFees.value}</span>
+                <span className="text-xs text-neutral-1 ml-0.5">
+                  ({item.burnsFees.percents})
+                </span>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  </div>
+);
---
+++ src/pages/explorer/BlockDetails/BlockDetails.tsx
@@ -0,0 +1,378 @@
+import { Link } from 'react-router-dom';
+
+import { Copy, Question, SquareArrowUpRight } from '@/assets/icons';
+import { Badge, Tabs, TabsContent, TabsList, TabsTrigger } from '@/ui-kit';
+
+enum TabsLabels {
+  Overview = 'Overview',
+  ConsensusInfo = 'Consensus Info',
+  MEVInfo = 'MEV Info',
+}
+
+const BLOCK = {
+  blockHeight: '19639603',
+  status: 'Finalized',
+  timestamp: '3 mins ago (Apr-11-2024 04:30:35 PM +UTC)',
+  transactionAction: ['137 transactions', '25 contract internal transactions'],
+  withdrawals: '16',
+  feeRecipient: {
+    value: 'beaverbuild',
+    time: '12 secs',
+  },
+  blockReward:
+    '0.056432172020433265 ETH (0 + 0.234848411539884275 - 0.17841623951945101)',
+  totalDifficulty: '58,750,003,716,598,352,816,469',
+  totalDifficultyBytes: '44,149 bytes',
+  gasUsed: {
+    value: '9,306,715',
+    percents: '31.02%',
+  },
+  gasLimit: '30,000,000',
+  feePerGas: {
+    eth: '0.000000019170699814 ETH',
+    gwei: '19.170699814 Gwei',
+  },
+  burntFee: '🔥 0.17841623951945101 ETH',
+  extraData: 'beaverbuild.org (Hex:0x6265617665726275696c642e6f7267)',
+  slot: '8841817',
+  epoch: '276306',
+  proposerIndex: '976305',
+  slotRootHash:
+    '0xa297e83a2f1b65f6fcda25a06fcb316457626279dd817a1e27916de568f1b565',
+  beaconDepositCount: '1430823',
+  slotGraffiti:
+    '0xd581afb764cc74b4d559d1bc073b61edb75402616ae679b5ec3c7829b83b4e76',
+  randaoReveal:
+    '0x86a093011dbe486b886bc8c993586523756b8f141d8a9ec5efead1445c45e9655f719c1b5c99cf5bd94c551b9ce3af370f676a20154b9a86fdd1b93485e057cd8346fa15dc06932fa71a64d526cc650c806e13feb244f37483b860c9c6be7045',
+  proposerFeeRecipient: '0xfFEE087852cb4898e6c3532E776e68BC68b1143B',
+  mevReward: '0.083469255526343207 ETH',
+  mevHash: '0x78ba2d2a71c6d5c150290a8f1951a889f80d2f628da7ca2d1a13ff78a2be8273',
+};
+
+export const BlockDetails = () => (
+  <div className="page-container">
+    <h1 className="text-h1 text-white font-semibold">Block</h1>
+    <Tabs defaultValue={TabsLabels.Overview} className="w-full mt-7.5">
+      <TabsList>
+        <TabsTrigger value={TabsLabels.Overview}>
+          {TabsLabels.Overview}
+        </TabsTrigger>
+        <TabsTrigger value={TabsLabels.ConsensusInfo}>
+          {TabsLabels.ConsensusInfo}
+        </TabsTrigger>
+        <TabsTrigger value={TabsLabels.MEVInfo}>
+          {TabsLabels.MEVInfo}
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value={TabsLabels.Overview}>
+        <div className="flex flex-col mt-7.5 w-full border border-neutral-2 p-5 rounded-lg bg-background-dark-grey text-base text-white">
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Block Height</p>
+            </div>
+            <div className="flex items-center w-full lg:w-4/6">
+              <p>{BLOCK.blockHeight}</p>
+            </div>
+          </div>
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Status</p>
+            </div>
+            <div className="flex items-center w-full lg:w-4/6">
+              <Badge
+                variant="outlined"
+                className="text-base px-4"
+                color={BLOCK.status === 'Finalized' ? 'success' : 'warning'}
+              >
+                {BLOCK.status}
+              </Badge>
+            </div>
+          </div>
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Timestamp</p>
+            </div>
+            <div className="flex items-center flex-wrap gap-2.5 w-full lg:w-4/6">
+              <p>{BLOCK.timestamp}</p>
+            </div>
+          </div>
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Proposed On</p>
+            </div>
+            <div className="flex items-center w-full lg:w-4/6">
+              <p>
+                Block proposed on slot{' '}
+                <Link to="#" className="text-blue hover:text-blue-darker">
+                  {BLOCK.slot},
+                </Link>
+                epoch{' '}
+                <Link to="#" className="text-blue hover:text-blue-darker">
+                  {BLOCK.epoch}
+                </Link>
+              </p>
+            </div>
+          </div>
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Transaction Action</p>
+            </div>
+            <div className="flex items-center flex-wrap gap-1 w-full lg:w-4/6">
+              <Link to="#" className="text-blue hover:text-blue-darker">
+                {BLOCK.transactionAction[0]}
+              </Link>{' '}
+              and{' '}
+              <Link to="#" className="text-blue hover:text-blue-darker">
+                {BLOCK.transactionAction[1]}
+              </Link>{' '}
+              in this block
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Withdrawals</p>
+            </div>
+            <div className="flex items-center w-full lg:w-4/6">
+              <Link to="#" className="text-blue hover:text-blue-darker">
+                {BLOCK.withdrawals} withdrawals
+              </Link>{' '}
+              in this block
+            </div>
+          </div>
+          <div className="border-t border-neutral-2 my-5" />
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Fee Recipient</p>
+            </div>
+            <div className="flex items-center w-full lg:w-4/6">
+              <Link
+                to="#"
+                className="text-blue hover:text-blue-darker ml-1 break-all"
+              >
+                {BLOCK.feeRecipient.value}
+              </Link>
+              <Copy
+                className="text-neutral-1 ml-2 min-w-[18px]"
+                width={18}
+                height={18}
+              />{' '}
+              in {BLOCK.feeRecipient.time}
+            </div>
+          </div>
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Block Reward</p>
+            </div>
+            <div className="flex items-center w-full lg:w-4/6">
+              <p>{BLOCK.blockReward}</p>
+            </div>
+          </div>
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Total Difficulty</p>
+            </div>
+            <div className="flex items-center w-full lg:w-4/6">
+              <p className="break-all">{BLOCK.totalDifficulty}</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Total Difficulty</p>
+            </div>
+            <div className="flex items-center w-full lg:w-4/6">
+              <p>{BLOCK.totalDifficultyBytes}</p>
+            </div>
+          </div>
+          <div className="border-t border-neutral-2 my-5" />
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Gas Used</p>
+            </div>
+            <div className="flex items-center flex-wrap gap-1 w-full lg:w-4/6">
+              <p>{BLOCK.gasUsed.value}</p>
+              <span className="text-neutral-1">({BLOCK.gasUsed.percents})</span>
+            </div>
+          </div>
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Gas Limit</p>
+            </div>
+            <div className="flex items-center flex-wrap gap-1 w-full lg:w-4/6">
+              <p>{BLOCK.gasLimit}</p>
+            </div>
+          </div>
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Base Fee Per Gas</p>
+            </div>
+            <div className="flex items-center flex-wrap gap-1 w-full lg:w-4/6">
+              <p>{BLOCK.feePerGas.eth}</p>
+              <span className="text-neutral-1">({BLOCK.feePerGas.gwei})</span>
+            </div>
+          </div>
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Burnt Fees</p>
+            </div>
+            <div className="flex items-center flex-wrap gap-1 w-full lg:w-4/6">
+              <p>{BLOCK.burntFee}</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Extra Data</p>
+            </div>
+            <div className="flex items-center flex-wrap gap-1 w-full lg:w-4/6">
+              <p className="break-all">{BLOCK.extraData}</p>
+            </div>
+          </div>
+        </div>
+      </TabsContent>
+      <TabsContent value={TabsLabels.ConsensusInfo}>
+        <div className="flex flex-col mt-7.5 w-full border border-neutral-2 p-5 rounded-lg bg-background-dark-grey text-base text-white">
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Slot</p>
+            </div>
+            <div className="flex flex-nowrap gap-1 items-center w-full lg:w-4/6">
+              <Link
+                to="#"
+                className="text-blue hover:text-blue-darker break-all"
+              >
+                {BLOCK.slot}
+              </Link>
+              <SquareArrowUpRight className="text-neutral-1" />
+            </div>
+          </div>
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Epoch</p>
+            </div>
+            <div className="flex flex-nowrap gap-1 items-center w-full lg:w-4/6">
+              <Link
+                to="#"
+                className="text-blue hover:text-blue-darker break-all"
+              >
+                {BLOCK.epoch}
+              </Link>
+              <SquareArrowUpRight className="text-neutral-1" />
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Proposer Index</p>
+            </div>
+            <div className="flex flex-nowrap gap-1 items-center w-full lg:w-4/6">
+              <Link
+                to="#"
+                className="text-blue hover:text-blue-darker break-all"
+              >
+                {BLOCK.proposerIndex}
+              </Link>
+              <SquareArrowUpRight className="text-neutral-1" />
+            </div>
+          </div>
+          <div className="border-t border-neutral-2 my-5" />
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Slot Root Hash</p>
+            </div>
+            <div className="flex items-center w-full lg:w-4/6">
+              <p className="break-all">{BLOCK.slotRootHash}</p>
+            </div>
+          </div>
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">
+                Beacon Chain Deposit Count
+              </p>
+            </div>
+            <div className="flex items-center w-full lg:w-4/6">
+              <p className="break-all">{BLOCK.beaconDepositCount}</p>
+            </div>
+          </div>
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Slot Graffiti</p>
+            </div>
+            <div className="flex items-center w-full lg:w-4/6">
+              <p className="break-all">{BLOCK.slotGraffiti}</p>
+            </div>
+          </div>
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Randao Reveal</p>
+            </div>
+            <div className="flex items-center w-full lg:w-4/6">
+              <p className="break-all">{BLOCK.randaoReveal}</p>
+            </div>
+          </div>
+        </div>
+      </TabsContent>
+      <TabsContent value={TabsLabels.MEVInfo}>
+        <div className="flex flex-col mt-7.5 w-full border border-neutral-2 p-5 rounded-lg bg-background-dark-grey text-base text-white">
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">Proposer Fee Recipient</p>
+            </div>
+            <div className="flex flex-wrap gap-1 items-center w-full lg:w-4/6">
+              <Link
+                to="#"
+                className="text-blue hover:text-blue-darker break-all"
+              >
+                {BLOCK.proposerFeeRecipient}
+              </Link>
+              (stakefish: Fee Recipient)
+            </div>
+          </div>
+          <div className="flex mb-5 flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">MEV Reward</p>
+            </div>
+            <div className="flex items-center w-full lg:w-4/6">
+              <p>{BLOCK.mevReward}</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 lg:flex-row">
+            <div className="flex items-center w-full lg:w-2/6">
+              <Question className="text-neutral-1 mr-1" />
+              <p className="text-neutral-1 text-lg">MEV Payout Txn Hash</p>
+            </div>
+            <div className="flex items-center w-full lg:w-4/6">
+              <Link
+                to="#"
+                className="text-blue hover:text-blue-darker break-all"
+              >
+                {BLOCK.mevHash}
+              </Link>
+            </div>
+          </div>
+        </div>
+      </TabsContent>
+    </Tabs>
+  </div>
+);
