a73e997eb764782c956e91bcba0a7c53c0220bd0
Vasyl Stasiuk <vasyl.stasiuk@wetelo.com>
2024-05-02 16:40:53 +0300

    fix: added styles for body, fixed issue with black text color

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import { design } from './theme/design';
-
-const applyGlobalStyles = () => {
-  document.body.style.background = design.colors.black;
-};
-
-applyGlobalStyles();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
