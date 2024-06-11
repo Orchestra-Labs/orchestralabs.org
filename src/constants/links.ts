ddd8a71ff6fde7dbbb1037b25d23661ef0dbf7b7
dblackstone1 <147945739+dblackstone1@users.noreply.github.com>
2024-06-11 15:00:01 -0400

    Fix: amend naming to uniform standard (#57)

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -6,32 +6,32 @@ export const MAIN_LAYOUT_LINKS = [
   {
     id: 1,
     label: 'Whitepaper',
-    url: ROUTES.WHITEPAPER,
+    href: ROUTES.WHITEPAPER,
   },
-  { id: 2, label: 'Documentation', url: ROUTES.DOCUMENTATION.ROOT },
-  { id: 3, label: 'Explorer', url: ROUTES.EXPLORER.ROOT },
+  { id: 2, label: 'Documentation', href: ROUTES.DOCUMENTATION.ROOT },
+  { id: 3, label: 'Explorer', href: ROUTES.EXPLORER.ROOT },
   {
     id: 4,
     label: 'Blogs',
     target: '_blank',
     rel: 'noopener noreferrer',
-    url: 'https://medium.com/@orchestra_labs',
+    href: 'https://medium.com/@orchestra_labs',
   },
 ];
 
 export const EXPLORER_LAYOUT_LINKS = [
-  { id: 1, label: 'Home', url: ROUTES.EXPLORER.ROOT },
-  { id: 2, label: 'Tokens', url: ROUTES.EXPLORER.TOKENS },
-  { id: 3, label: 'Blocks', url: ROUTES.EXPLORER.BLOCKS },
-  { id: 4, label: 'Transactions', url: ROUTES.EXPLORER.TRANSACTIONS },
+  { id: 1, label: 'Home', href: ROUTES.EXPLORER.ROOT },
+  { id: 2, label: 'Tokens', href: ROUTES.EXPLORER.TOKENS },
+  { id: 3, label: 'Blocks', href: ROUTES.EXPLORER.BLOCKS },
+  { id: 4, label: 'Transactions', href: ROUTES.EXPLORER.TRANSACTIONS },
 ];
 
 export const SOCIAL_LINKS = [
-  { id: 1, url: 'https://twitter.com/orchestra_labs', Icon: XIcon },
-  { id: 2, url: 'https://t.me/+xFieHCYYyx41NGQx', Icon: Telegram },
-  { id: 3, url: 'https://github.com/Orchestra-Labs', Icon: GitHub },
-  { id: 4, url: 'https://discord.gg/qEBPwncrSV', Icon: Discord },
-  { id: 5, url: 'https://linktr.ee/OrchestraLabs', Icon: LinkTree },
+  { id: 1, href: 'https://twitter.com/orchestra_labs', Icon: XIcon },
+  { id: 2, href: 'https://t.me/+xFieHCYYyx41NGQx', Icon: Telegram },
+  { id: 3, href: 'https://github.com/Orchestra-Labs', Icon: GitHub },
+  { id: 4, href: 'https://discord.gg/qEBPwncrSV', Icon: Discord },
+  { id: 5, href: 'https://linktr.ee/OrchestraLabs', Icon: LinkTree },
 ];
 
 export const EXPLORER_FOOTER_LINKS = [
