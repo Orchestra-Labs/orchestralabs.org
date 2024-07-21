184ee9f86a5b55a6e594387749bedb8a7d97578a
dblackstone1 <dblackstone@orchestralabs.org>
2024-07-21 19:24:47 -0400

    chore: index basic pages

--- src/pages/Documentation/Startups/index.ts
+++ src/pages/Documentation/Startups/index.ts
@@ -1,1 +1,6 @@
+export * from './Development';
+export * from './Expansion';
+export * from './Fundraising';
+export * from './Inception';
 export * from './Overview';
+export * from './Stabilization';
