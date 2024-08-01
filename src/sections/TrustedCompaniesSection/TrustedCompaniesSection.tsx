fbe4d8b933a7b8dc042bd4106c555681680fe9a4
NodeSync.Top is Trusted PoS Validator <124766822+nodesynctop@users.noreply.github.com>
2024-08-01 11:20:18 +0700

    Update TrustedCompaniesSection.tsx

--- src/sections/TrustedCompaniesSection/TrustedCompaniesSection.tsx
+++ src/sections/TrustedCompaniesSection/TrustedCompaniesSection.tsx
@@ -36,6 +36,7 @@ import syanodes from '@/assets/images/syanodes.png';
 import jhonswg from '@/assets/images/jhonswg.png';
 import cryptonodeid from '@/assets/images/cryptonodeid.png';
 import nodestake from '@/assets/images/nodestake.png';
+import nodesync from '@/assets/images/nodesync.png';
 
 type Company = {
   id: number;
@@ -246,6 +247,12 @@ const COMPANIES: Company[] = [
     id: 33,
     name: 'NodeStake',
     src: nodestake,
+    logoClass: 'max-h-10',
+  },
+  {
+    id: 34,
+    name: 'NodeSync',
+    src: nodesync,
     logoClass: 'max-h-10',
   },
 ];
