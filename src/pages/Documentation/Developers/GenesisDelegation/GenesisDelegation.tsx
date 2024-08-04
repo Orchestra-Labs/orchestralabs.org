b2fd589cc2f5357164bd8c26dbb93cfd9c4a7770
dblackstone1 <dblackstone@orchestralabs.org>
2024-08-04 18:40:22 -0400

    chore: genesis delegations title fix

--- src/pages/Documentation/Developers/GenesisDelegation/GenesisDelegation.tsx
+++ src/pages/Documentation/Developers/GenesisDelegation/GenesisDelegation.tsx
@@ -64,7 +64,7 @@ export const GenesisDelegation = () => (
     <div className="page-container my-0 flex flex-col-reverse md:flex-row pt-[26px] gap-5 lg:gap-10 xl:gap-15 pl-25px md:pl-12 xl:pl-17">
       <div className="my-0 pt-6 pt-8 lg:pt-11 pb-9 md:pb-14 xl:pb-19 text-body-md text-grey">
         <h1 className="text-white font-semibold text-h2 md:text-h1 xl:text-display2">
-          Orchestra Labs Genesis Delegation Program
+          Genesis Delegations
         </h1>
         <p className="mt-10">
           The Genesis Delegation Program is one of the programs through which
