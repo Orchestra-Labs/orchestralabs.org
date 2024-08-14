9b15301ac015511accf949ed85dbf06a0957c2b1
root <root@v2202407230294279854.ultrasrv.de>
2024-08-14 17:15:08 +0200

    Add X1NODE with testnet-focused description, logo, guides, snapshots, genesis files, and address books

---
+++ src/pages/Explorers.tsx
@@ -0,0 +1,8 @@
+const explorers = [
+  ...existingExplorers,
+  {
+    name: 'X1NODE Explorer',
+    url: 'https://explorer-testnet.x1node.xyz',
+    description: 'An explorer from one of our supporting validators.',
+  },
+];
