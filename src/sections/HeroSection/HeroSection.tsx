08ed6f4f76d210ec0c59a68dd9c6ea7c14d90215
dblackstone1 <dblackstone@orchestralabs.org>
2024-10-19 15:07:16 -0400

    feat: change hero section dialog

--- src/sections/HeroSection/HeroSection.tsx
+++ src/sections/HeroSection/HeroSection.tsx
@@ -11,7 +11,7 @@ export const HeroSection = () => (
     <div className="flex justify-center items-center min-h-[inherit] relative z-[1] px-25px md:px-6">
       <div className="flex flex-col max-w-[882px] text-center items-center gap-4 mt-[-50%] md:-mt-40 xl:-mt-[120px]">
         <h1 className="font-semibold text-white text-h4 md:text-h2/[56px] xl:text-display2">
-          <p>Real Assets.</p> <p>Perfect Harmony.</p>
+          <p>Your Money</p> <p>Your Symphony</p>
         </h1>
         <p className="text-grey text-body-md md:text-body-lg xl:text-body-xl mt-5">
           {/* Releasing soon via IDO */}
