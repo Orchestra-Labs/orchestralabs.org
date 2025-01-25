750852132d49aadeba8cf29b3d9089aaf7f362a3
dblackstone1 <dblackstone@orchestralabs.org>
2025-01-24 21:44:01 -0500

    feat: modify contributions page

--- src/pages/Documentation/Developers/Contributing/Contributing.tsx
+++ src/pages/Documentation/Developers/Contributing/Contributing.tsx
@@ -5,14 +5,21 @@ import { Link } from 'react-router-dom';
 import { ROUTES } from '@/config/routes';
 import { ImageModal } from '@/components/ImageModal';
 
-import addDirectoryForNewPage from '@/assets/images/add-directory-for-new-page.png';
-import addToNavigationPages from '@/assets/images/add-to-navigation-pages.png';
 import commitStyle from '@/assets/images/commit-style.png';
+import githubIssues from '@/assets/images/github-issues.png';
 
 const HEADINGS = {
   1: {
-    id: 'contributing-to-site',
-    label: 'Adding Pages to the Site',
+    id: 'reporting-issues',
+    label: 'Reporting Issues',
+  },
+  2: {
+    id: 'readying-repository',
+    label: 'Readying the Repository',
+  },
+  3: {
+    id: 'commit-style',
+    label: 'Commit Style',
   },
 };
 
@@ -22,6 +29,16 @@ const NAVIGATION_ITEMS: NavItem[] = [
     label: HEADINGS[1].label,
     href: `#${HEADINGS[1].id}`,
   },
+  {
+    id: '2',
+    label: HEADINGS[2].label,
+    href: `#${HEADINGS[2].id}`,
+  },
+  {
+    id: '3',
+    label: HEADINGS[3].label,
+    href: `#${HEADINGS[3].id}`,
+  },
 ];
 
 export const Contributing = () => (
@@ -30,8 +47,9 @@ export const Contributing = () => (
     <div className="page-container my-0 flex flex-col-reverse md:flex-row pt-[26px] gap-5 lg:gap-10 xl:gap-15 pl-25px md:pl-12 xl:pl-17">
       <div className="my-0 pt-6 pt-8 lg:pt-11 pb-9 md:pb-14 xl:pb-19 text-body-md text-grey">
         <h1 className="text-white font-semibold text-h2 md:text-h1 xl:text-display2">
-          Contributing as a Developer
+          Contributing as a Dev
         </h1>
+        {/* TODO: split into two sections.  how to add issues, how to get started.  include recommendation to add issues to amend documentation if there is either outdated or insufficient documentation to get started */}
         <p className="mt-10">
           There are several ways to contribute as a developer. If you're
           starting a new business and you're not sure how to start, check out
@@ -60,47 +78,47 @@ export const Contributing = () => (
           </h2>
         </div>
         <p className="mt-5">
-          When contributing to the site, if you're adding a page, add a
-          directory for it under the appropriate category, as shown in the image
-          below. Be sure to update the index.ts files both in that directory and
-          in the directory immediately above it, as those files make reference
-          imports easier.
+          There are two ways to report issues. The first is to report it to us
+          directly in the <strong>#support-chat</strong> channel in{' '}
+          <Link
+            className="text-blue hover:text-blue-darker"
+            target="_blank"
+            to={SOCIAL_LINKS[3].href}
+          >
+            Discord
+          </Link>
+          . The second is to create a Github issue by navigating to your chosen
+          repository and selecting the issues tab, then clicking the "New Issue"
+          button.
         </p>
-        <p className="mt-5">
-          <strong>Note: </strong>If your addition would be more appropriate
-          under a new category, add that instead.
-        </p>
-        <ImageModal
-          imageSrc={addDirectoryForNewPage}
-          altText="Add Directory for New Page"
-        />
+        <ImageModal imageSrc={githubIssues} altText="GitHub Issues" />
 
+        <div id={HEADINGS[2].id} className="mt-15">
+          <h2 className="text-h4 md:text-h3 xl:text-h1 font-semibold text-white">
+            {HEADINGS[2].label}
+          </h2>
+        </div>
         <p className="mt-5">
-          Be sure to add your new page to the three navigation pages we have:
+          Every one of our repositories has a top-level readme with instructions
+          on getting started. Go to that file or check the first page of your
+          chosen repository to find instructions on how to get started.
         </p>
-        <ul className="list-disc pl-5">
-          <p>- routes.ts: Includes routing information for the browser</p>
-          <p>- Router.tsx: Connects a path to the component.</p>
-          <p className="mb-5">
-            - documentationNavigation.ts: Includes all the link information for
-            the documentation pages.
-          </p>
-        </ul>
-        <ImageModal
-          imageSrc={addToNavigationPages}
-          altText="Add to Navigation Pages"
-        />
 
+        <div id={HEADINGS[3].id} className="mt-15">
+          <h2 className="text-h4 md:text-h3 xl:text-h1 font-semibold text-white">
+            {HEADINGS[3].label}
+          </h2>
+        </div>
         <p className="mt-5">
-          When ready with your contributions to the site's code, make a pull
-          request. Be sure to follow the{' '}
+          When ready with your contributions to one of our repositories, make a
+          pull request. Be sure to follow the{' '}
           <Link
             className="text-blue hover:text-blue-darker"
             to="https://www.conventionalcommits.org/en/v1.0.0-beta.2/"
           >
             conventional commits
           </Link>{' '}
-          style, as it helps with the squash merge commit descriptions.
+          style, as it helps with the squash-merge commit descriptions.
         </p>
         <ImageModal imageSrc={commitStyle} altText="Commit Style" />
       </div>
